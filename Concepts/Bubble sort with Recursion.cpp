4c50582535aa12483944beae6895e645f1a61748
shovon-coder <114784022+shovon-coder@users.noreply.github.com>
2022-11-20 00:17:01 +0600

    bubbleSort

---
+++ Concepts/Bubble sort with Recursion.cpp
@@ -0,0 +1,34 @@
+// File Name: Bubble sort with Recursion
+// Date: 2022-11-19
+// Time: 23:56:22
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void sortArray(int arr[],int n)
+{
+    if(n == 0 || n == 1) return;
+
+    for(int i=0;i<n-1;i++)
+    {
+        if(arr[i] > arr[i+1])
+        {
+            swap(arr[i],arr[i+1]);
+        }
+    }
+
+    sortArray(arr,n-1);
+
+}
+
+int main(){
+    
+  int arr[5]={2,5,1,6,9};
+  sortArray(arr,5);
+
+  for(int i=0;i<5;i++)
+  {
+    cout<<arr[i]<< " ";
+  }
+  return 0;
+}
