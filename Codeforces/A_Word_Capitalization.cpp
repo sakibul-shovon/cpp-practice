bc140d8810d18997caf2e8cee9caf15de5701e83
shovon-coder <114784022+shovon-coder@users.noreply.github.com>
2022-12-09 03:24:12 +0600

    add

---
+++ Codeforces/A_Word_Capitalization.cpp
@@ -0,0 +1,14 @@
+// File Name: A_Word_Capitalization.cpp
+// Date: 2022-12-05
+// Time: 23:03:29
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    string s;
+    cin>>s;
+    s[0]=toupper(s[0]);
+    cout<<s<<endl;
+    return 0;
+}
